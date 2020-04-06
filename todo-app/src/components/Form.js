d7f00f12c52bd5478082714a8d799ddfabd5a01a
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-06 15:12:10 -0700

    finished updating

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,39 +1,8 @@
-import React, { useEffect } from "react";
-import { withFormik, Formik, Form, Field } from "formik";
+import React from "react";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 
 import uuid from "uuid/v4";
-
-// const MainForm = ({ createTodo, status, errors, touched }) => {
-//   useEffect(() => {
-//     status && createTodo(status);
-//   }, [status, createTodo]);
-
-//   return (
-//     <Form>
-//       <Field type="text" name="todo" id="todo" />
-//       {errors.todo && touched.todo && <p>{errors.todo}</p>}
-//       <button type="submit">Add</button>
-//     </Form>
-//   );
-// };
-
-// export default withFormik({
-//   mapPropsToValues: () => ({
-//     todo: ""
-//   }),
-//   validationSchema: yup.object().shape({
-//     todo: yup.string().required("please enter a value")
-//   }),
-//   handleSubmit: (values, { resetForm, setStatus }) => {
-//     const obj = {
-//       id: uuid(),
-//       todo: values.todo
-//     };
-//     setStatus(obj);
-//     resetForm();
-//   }
-// })(MainForm);
 
 const validationSchema = yup.object().shape({
   todo: yup.string().required("please enter a value"),
